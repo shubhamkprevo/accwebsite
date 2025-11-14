f0bc65a5ee3cc179ed0eb73129fdbfe0d3efeb27
shubhamkprevo <shubham.k@prevoyancesolutions.com>
2025-11-14 20:41:30 +0530

    comment added in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <div>
        <Header/>
-      
+      {/* my routes */}
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
